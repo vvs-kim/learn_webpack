d405bf48ebd76356726dba773b4e52278623f9ab
linkrws <wooseok.kim@linkr.social>
2022-10-06 18:15:01 +0900

    chore: css webpack loader 설정

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,15 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  module: {
+    rules: [
+      // loader는 오른쪽부터 왼쪽으로 불러온다.
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
   plugins: [
     new HTMLWebpackPlugin({
       template: path.resolve(__dirname, "source", "index.html"),
